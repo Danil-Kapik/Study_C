5b70347e3877263d8e01566d70b24b83589d5898
Daniil-Kapik <dann_kap@mail.ru>
2023-09-01 18:09:13 +0300

    feat: add task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+//           а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Введите число n:");
+if (int.TryParse(Console.ReadLine(), out int n) && n >= 1)
+{
+    Console.WriteLine("Четные числа от 1 до n:");
+
+    for (int i = 2; i <= n; i += 2)
+    {
+        Console.Write(i + " ");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка: Введите корректное положительное число.");
+}
