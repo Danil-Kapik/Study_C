3232e55dd88427c503d9ecb8e25f98d55378e391
Daniil-Kapik <dann_kap@mail.ru>
2023-09-06 14:46:13 +0300

    feat: add task_13

---
+++ task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+//            заданного числа или сообщает, что третьей цифры нет.
+
+
+
+Console.WriteLine("Введите число:");
+string str = Console.ReadLine();
+if (str != null && str.Length >= 3 && int.TryParse(str, out int n))
+{
+    Console.WriteLine($"Введенное число - {str}");
+    Console.Write($"Третья цифра этого числа = {str[2]}");
+}
+else
+{
+    Console.WriteLine($"Ошибка: Третьей цифры нет -- {str}");
+}
