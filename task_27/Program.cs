554cc4e84ac97e32accfac198cfb52c8ce510e7a
Daniil-Kapik <dann_kap@mail.ru>
2023-09-16 20:31:10 +0300

    feat: add task_27

---
+++ task_27/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 27: Напишите программу, которая принимает на 
+//            вход число и выдаёт сумму цифр в числе.
+
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Введите число:");
+        if (int.TryParse(Console.ReadLine(), out int n))
+        {
+            int result = SumOfDigits(n);
+            Console.WriteLine($"Сумма цифр числа {n} = {result}");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: Введите корректное число n.");
+        }
+    }
+
+    static int SumOfDigits(int n)
+    {
+        int sum = 0;
+        n = Math.Abs(n);
+        for (; n > 0; n /= 10)
+        {
+            int digit = n % 10;
+            sum += digit;
+        }
+
+        return sum;
+    }
+}
