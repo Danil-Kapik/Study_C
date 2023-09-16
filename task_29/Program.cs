225fd2bbe821597b61827f38e7d536d34509acc7
Daniil-Kapik <dann_kap@mail.ru>
2023-09-16 20:58:55 +0300

    feat: add task_29

---
+++ task_29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+//            из 8 элементов и выводит их на экран.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+        Random random = new Random();
+        int[] numbers = new int[8];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 101);
+        }
+        Console.WriteLine("Элементы массива:");
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
