760df874fbcdd59db4a0a1fd2e8f6b873754424e
Daniil-Kapik <dann_kap@mail.ru>
2023-09-24 01:34:45 +0300

    feat: add task_41

---
+++ LESSON_6/task_41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//            Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите числа, разделенные пробелами: ");
+        string input = Console.ReadLine();
+        string[] numberStrings = input.Split(' ');
+        int positiveCount = 0;
+
+        foreach (string numberString in numberStrings)
+        {
+            if (double.TryParse(numberString, out double number))
+            {
+                if (number > 0)
+                {
+                    positiveCount++;
+                }
+            }
+        }
+
+        Console.WriteLine($"Количество чисел больше нуля: {positiveCount}");
+    }
+}
