627e7c9d1bb0f75305735bcb5b3b3e4683c08894
Daniil-Kapik <dann_kap@mail.ru>
2023-10-01 15:32:58 +0300

    feat: add task_60

---
+++ LESSON_7/task_60/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных 
+//               чисел. Напишите программу, которая будет построчно выводить 
+//               массив, добавляя индексы каждого элемента.
+//               Массив размером 2 x 2 x 2
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int xSize = 2;
+        int ySize = 2;
+        int zSize = 2;
+
+        int[,,] array = GenerateUniqueTwoDigitArray(xSize, ySize, zSize);
+        PrintArrayWithIndices(array);
+    }
+
+    static int[,,] GenerateUniqueTwoDigitArray(int xSize, int ySize, int zSize)
+    {
+        Random random = new Random();
+        int[,,] array = new int[xSize, ySize, zSize];
+        bool[] usedNumbers = new bool[100];
+
+        for (int i = 0; i < xSize; i++)
+        {
+            for (int j = 0; j < ySize; j++)
+            {
+                for (int k = 0; k < zSize; k++)
+                {
+                    int randomNumber;
+                    do
+                    {
+                        randomNumber = random.Next(10, 100);
+                    } while (usedNumbers[randomNumber]);
+
+                    array[i, j, k] = randomNumber;
+                    usedNumbers[randomNumber] = true;
+                }
+            }
+        }
+
+        return array;
+    }
+
+    static void PrintArrayWithIndices(int[,,] array)
+    {
+        int xSize = array.GetLength(0);
+        int ySize = array.GetLength(1);
+        int zSize = array.GetLength(2);
+
+        for (int i = 0; i < xSize; i++)
+        {
+            for (int j = 0; j < ySize; j++)
+            {
+                for (int k = 0; k < zSize; k++)
+                {
+                    Console.Write($"{array[i, j, k]}({i},{j},{k})\t");
+                    
+                }
+                Console.Write('\n');
+            }
+        }
+    }
+}
+
