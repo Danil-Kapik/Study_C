0ec8005dfb0e8b6df1e281abe1ca3371d432f466
Daniil-Kapik <dann_kap@mail.ru>
2023-10-01 19:59:49 +0300

    feat: add three tasks for lesson_9

---
+++ LESSON_9/task_64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая 
+//            выведет все натуральные числа в промежутке от N до 1. 
+//            Выполнить с помощью рекурсии.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+        PrintNaturalNumbers(N);
+    }
+
+    static void PrintNaturalNumbers(int N)
+    {
+        if (N >= 1)
+        {
+            Console.Write(N + " ");
+            PrintNaturalNumbers(N - 1);
+        }
+    }
+}
---
+++ LESSON_9/task_66/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+//            найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        if (M > N)
+        {
+            Console.WriteLine("Ошибка: M должно быть меньше или равно N.");
+        }
+        else
+        {
+            int sum = CalculateSum(M, N);
+            Console.WriteLine($"Сумма натуральных чисел в промежутке от {M} до {N}: {sum}");
+        }
+    }
+
+    static int CalculateSum(int start, int end)
+    {
+        if (start > end)
+        {
+            return 0;
+        }
+        else
+        {
+            return start + CalculateSum(start + 1, end);
+        }
+    }
+}
